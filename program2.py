d0718bad949a12a32f69968693e18754f27083d2
springymate <yashcooljain21@gmail.com>
2024-04-25 14:20:01 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         total = 0
-        # 
+        # loop through
         for i in range(len(s)):
             if i < len(s) - 1 and roman_values[s[i]] < roman_values[s[i + 1]]:
                 total -= roman_values[s[i]]
